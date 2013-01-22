3f758eed047b82f31ee4d51615848ac1179d98ff
alejo <alejo.ruiz.robles@gmail.com>
2013-01-22 10:44:46 +0100

    i18n

--- config/routes.rb
+++ config/routes.rb
@@ -2,37 +2,43 @@ SampleApp::Application.routes.draw do
   
   #resources :users
 
-  resources :users do
-    member do
-      get :following, :followers
-    end 
+  scope '(:locale)' do
+
+    resources :users do
+      member do
+        get :following, :followers
+      end 
+    end
+
+
+    resources :sessions, only: [:new, :create, :destroy]
+    resources :microposts, only: [:create, :destroy]
+    resources :relationships, only: [:create, :destroy]
+
+    match '/signup', to: 'users#new'
+    match '/signin', to: 'sessions#new'
+    match '/signout', to: 'sessions#destroy', via: :delete
+
+    #get "static_pages/home"
+
+    #root :to => 'users#alejo'
+    root :to => 'static_pages#home'
+
+    #get "static_pages/help"
+
+    match '/help', to: 'static_pages#help'  
+
+    #get "static_pages/about"
+
+    match '/about', to: 'static_pages#about'
+
+    #get "static_pages/contact"
+
+  
+
+    match '/contact', to: 'static_pages#contact'
+
   end
-
-
-  resources :sessions, only: [:new, :create, :destroy]
-  resources :microposts, only: [:create, :destroy]
-  resources :relationships, only: [:create, :destroy]
-
-  match '/signup', to: 'users#new'
-  match '/signin', to: 'sessions#new'
-  match '/signout', to: 'sessions#destroy', via: :delete
-
-  #get "static_pages/home"
-
-  #root :to => 'users#alejo'
-  root :to => 'static_pages#home'
-
-  #get "static_pages/help"
-
-  match '/help', to: 'static_pages#help'  
-
-  #get "static_pages/about"
-
-  match '/about', to: 'static_pages#about'
-
-  #get "static_pages/contact"
-
-  match '/contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
